2e9c023c2340901b5af6ba38968edb47ab3a8bdb
Hema Kasthuri. R <84775393+Hema-Kasthuri-R@users.noreply.github.com>
2021-05-28 17:23:24 +0530

    Create Arithmetic.java
    Simple program to perform arithmetic operations

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+public class Arithmetic{
+
+     public static void main(String []args){
+         int a = 10;
+         int b = 5;
+         int sum,dif,pro,div;
+         sum = a+b;
+         dif = a-b;
+         pro = a*b;
+         div = a/b;
+        System.out.println("sum = "+ sum + "\ndifference = " + dif + "\nProduct = " + pro + "\nQuotient = " + div );
+     }
+}
