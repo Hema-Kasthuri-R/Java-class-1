12b4b4d93fec7888062757b767227c5333aa0b8d
Hema Kasthuri. R <84775393+Hema-Kasthuri-R@users.noreply.github.com>
2021-05-28 17:25:58 +0530

    Create dnajoin.java
    Simple program to concatenate two strings

---
+++ dnajoin.java
@@ -0,0 +1,8 @@
+public class dnajoin{
+
+     public static void main(String []args){
+        String dna1 = "ATGCCATTAAACCA";
+        String dna = "ATAAAAAAAACCATAAATAATTTTACCA";
+        System.out.println(dna1+dna2);
+     }
+}
